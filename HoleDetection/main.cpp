990dfeec58e7541f086d62f4da8d759dcd442013
Mayant Mukul <mayantmukul@hotmail.com>
2020-11-17 23:29:33 +0530

    Fit circles to the extracted boundary with RANSAC

--- HoleDetection/main.cpp
+++ HoleDetection/main.cpp
@@ -2,6 +2,13 @@
 
 #include "detection.h"
 
+#include <pcl/ModelCoefficients.h>
+#include <pcl/sample_consensus/ransac.h>
+#include <pcl/sample_consensus/sac_model_circle3d.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+
 int main(int argc, char* argv[])
 {
     if (argc != 2)
@@ -18,12 +25,48 @@ int main(int argc, char* argv[])
     // Get clusters in the boundary
     auto clusters = ExtractEuclideanClusters(boundary);
 
-    int j = 1;
-    for (auto cluster : clusters)
+    // The largest of these clusters is probably the exterior boundary, so ignore that
+    int max = clusters[0]->size();
+    int max_idx = 0;
+    for (int i = 1; i < clusters.size(); ++i)
     {
-        std::string filename = "boundary" + std::to_string(j) + ".txt";
-        WriteOut(cluster, filename);
-        ++j;
+        if (clusters[i]->size() > max)
+        {
+            max = clusters[i]->size();
+            max_idx = i;
+        }
+    }
+
+    for (int i = 0; i < clusters.size(); ++i)
+    {
+        if (i == max_idx) continue;
+
+        // Fit circle
+        auto model = std::make_shared<pcl::SampleConsensusModelCircle3D<Point>>(clusters[i]);
+        auto coefficients = std::make_shared<pcl::ModelCoefficients>();
+        auto inliers = std::make_shared<pcl::PointIndices>();
+        
+        // Create the segmentation object
+        pcl::SACSegmentation<Point> seg;
+        seg.setOptimizeCoefficients (true);
+        seg.setModelType (pcl::SACMODEL_CIRCLE3D);
+        seg.setMethodType (pcl::SAC_RANSAC);
+        seg.setDistanceThreshold (0.01);
+        seg.setInputCloud (clusters[i]);
+        seg.segment (*inliers, *coefficients);
+
+        if (inliers->indices.size () == 0)
+        {
+            std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
+            return (-1);
+        }
+
+        std::cout << "\nHole " << i << ":\n";
+        std::cout << "Center: ("<< coefficients->values[0] << ", " << coefficients->values[1] << ", " << coefficients->values[2] << ")\n";
+        std::cout << "Radius: " << coefficients->values[3] << std::endl;
+        std::cout << "Normal: ("<< coefficients->values[4] << ", " << coefficients->values[5] << ", " << coefficients->values[6] << ")\n";
+
+        WriteOut(clusters[i], "hole" + std::to_string(i) + ".txt");
     }
 
     return 0;
